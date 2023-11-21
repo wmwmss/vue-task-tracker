0549508511af72ca5b8eb4a27bb3fe8bdcfce851
wmwmss <xu.han@rhsmith.umd.edu>
2023-11-21 14:45:26 -0500

    Project Init 231121

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <header>
+        <h1 style="text-align: center;">Task Tracker of {{ title }}</h1>
+        <Button :text="showAddTask ? 
+        'Close':'Add Task'" 
+        @btn-click="$emit('toggle-add-task')" 
+        :color="showAddTask ? 'gold' : 'green'"></Button>
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+        showAddTask: Boolean
+    },
+    components: {
+        Button
+    }
+}
+</script>
+
+<style scoped>
+ header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+ }
+</style>
